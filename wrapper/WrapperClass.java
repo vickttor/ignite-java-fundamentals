af5e2d22925d3a30730061a5202a896db5ef869b
Victor Silva <victor.silvaPC@hotmail.com>
2023-10-10 20:23:12 -0300

    feat: wrapper classes

---
+++ wrapper/WrapperClass.java
@@ -0,0 +1,19 @@
+package fundamentals.wrapper;
+
+public class WrapperClass {
+
+    public static void main(String[] args) {
+        // in primitive types we can't pass null as value.
+        // In wrapper classes we can assign null
+
+        int intType = 1;
+        float floatType = 2.0f;
+        boolean booleanType = false;
+
+        // int - char
+        Float floatType2 = 1F;
+        Boolean booleanType2 = false;
+        Integer intType2 = 2;
+
+    }
+}
