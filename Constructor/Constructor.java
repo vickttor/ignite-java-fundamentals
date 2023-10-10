cc5b0d3287dbedf265a2f57ca8d9e74946958403
Victor Silva <victor.silvaPC@hotmail.com>
2023-10-10 20:16:15 -0300

    feat: constructors

---
+++ Constructor/Constructor.java
@@ -0,0 +1,11 @@
+package fundamentals.Constructor;
+
+public class Constructor {
+
+    // overloads
+    public Constructor(int number, String text) {}
+    public Constructor(int number) {}
+    public Constructor(String text){}
+    public Constructor(){}
+
+}
